c6ae08cc4421043dec97598256756fb3b47c6a9e
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-03-05 09:37:51 +0700

    Initial Commit

---
+++ TimeLineAnimation/TimeLineAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLineAnimationApp.swift
+//  TimeLineAnimation
+//
+//  Created by Naufal Adli on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeLineAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
